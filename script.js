622d774c47e8a8dc2e930d07b4cd6e962aa526d2
ToadSupremacist <104451465+ToadSupremacist@users.noreply.github.com>
2023-01-23 19:44:29 +0500

    добавила отдельную страницу для предприятий
    +убрала анимации, поправила отступы и размеры, и тд и тп

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const header = document.querySelector(".header")
+// const header = document.querySelector(".header")
 
-window.addEventListener("scroll", (event) => {
-    header.classList.add("header--thin")
-})
+// window.addEventListener("scroll", (event) => {
+//     header.classList.add("header--thin")
+// })
